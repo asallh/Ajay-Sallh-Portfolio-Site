fd520f9528dda5876e9383d193f9f8a977ab32a4
asallh <ajaypalsallh@gmail.com>
2024-10-24 16:22:35 -0600

    cards now working

--- src/app/views/Projects.tsx
+++ src/app/views/Projects.tsx
@@ -1,10 +1,18 @@
 import ProjectCard from "@/components/ProjectCard";
-import { Box } from "@chakra-ui/react"
+import { Box } from "@chakra-ui/react";
+import { projects } from "../data/projectData";
 
 export default function Projects() {
   return (
-  <Box>
-    <ProjectCard />
-  </Box>
+    <Box>
+      {projects.map((project, index) => (
+        <ProjectCard
+          key={index}
+          title={project.title}
+          details={project.details}
+          techStack={project.techStack}
+        />
+      ))}
+    </Box>
   );
-};
+}
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,22 +1,31 @@
 import { Center, HStack, Text } from "@chakra-ui/react";
 
-export default function ProjectCard() {
+interface CardProps {
+    title: string;
+    details: string;
+    techStack: string[];
+}
+
+export default function ProjectCard({title, details, techStack} : CardProps) {
   return (
     <div className="w-80 h-64 bg-gray-0 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 m-16 flex flex-col justify-between">
       <div>
         <Center padding={4}>
-          <Text fontSize="xl">Project Name</Text>
+          <Text fontSize="xl">{title}</Text>
         </Center>
         <Center>
-          <Text>Details</Text>
+          <Text>{details}</Text>
         </Center>
       </div>
       {/* This div will be pushed to the bottom of the card */}
       <div className="flex justify-center mt-auto pb-4">
         <Center>
           <HStack>
-            <Text>Icons</Text>
+            {/* <Text>Icons</Text> */}
             {/* Add your icons here */}
+            {techStack.map((tech, index) => (
+              <Text key={index}>{tech}</Text>
+            ))}
           </HStack>
         </Center>
       </div>
