af2b9e0ecd614059240df65e4e1c4b8bdf805fa1
asallh <ajaypalsallh@gmail.com>
2024-10-24 15:41:07 -0600

    basic layout

---
+++ src/app/data/projectData.ts
@@ -0,0 +1,20 @@
+export const projects = [
+    {
+        title: 'Project',
+        details: 'This was my project',
+        techStack: [
+            'TypeScript',
+            'React',
+            'MongoDB'
+        ]
+    },
+    {
+        title: 'Project2',
+        details: 'This was my second project',
+        techStack: [
+            'FastApi',
+            'React',
+            'MongoDB'
+        ]
+    },
+];
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,29 +1,25 @@
-import { Card, Image, Button, Text } from "@chakra-ui/react";
+import { Center, HStack, Text } from "@chakra-ui/react";
 
 export default function ProjectCard() {
   return (
-    <Card.Root
-      maxW="sm"
-      overflow="hidden"
-    >
-      <Image
-        src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-        alt="Green double couch with wooden legs"
-      />
-      <Card.Body gap="2">
-        <Card.Title>Living room Sofa</Card.Title>
-        <Card.Description>
-          This sofa is perfect for modern tropical spaces, baroque-inspired
-          spaces.
-        </Card.Description>
-        <Text textStyle="2xl" fontWeight="medium" letterSpacing="tight" mt="2">
-          $450
-        </Text>
-      </Card.Body>
-      <Card.Footer gap="2">
-        <Button variant="solid">Buy now</Button>
-        <Button variant="ghost">Add to cart</Button>
-      </Card.Footer>
-    </Card.Root>
+    <div className="w-80 h-64 bg-gray-0 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-10 border border-gray-100 m-16 flex flex-col justify-between">
+      <div>
+        <Center padding={4}>
+          <Text fontSize="xl">Project Name</Text>
+        </Center>
+        <Center>
+          <Text>Details</Text>
+        </Center>
+      </div>
+      {/* This div will be pushed to the bottom of the card */}
+      <div className="flex justify-center mt-auto pb-4">
+        <Center>
+          <HStack>
+            <Text>Icons</Text>
+            {/* Add your icons here */}
+          </HStack>
+        </Center>
+      </div>
+    </div>
   );
 }
