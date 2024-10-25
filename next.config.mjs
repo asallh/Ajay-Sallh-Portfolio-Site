bcdbe7576ffe0201700a28c03d24983da7d2dbf3
asallh <ajaypalsallh@gmail.com>
2024-10-25 08:45:31 -0600

    need to keep this like this for deployment compiling reasons lol

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export', 
-    basePath: '/portfolio', 
-    assetPrefix: '/portfolio/', 
-    images: {
-      unoptimized: true, 
-    },
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
