8e7da9c289f7a19f452ca85327fb9b8c7f10b30d
asallh <ajaypalsallh@gmail.com>
2024-10-25 09:07:24 -0600

    updated image location

--- src/app/views/Hero.tsx
+++ src/app/views/Hero.tsx
@@ -1,6 +1,6 @@
 import { Box, Center, Flex, HStack, Text } from "@chakra-ui/react";
 import Image from "next/image";
-import profilepic from "../assets/memeoji.png";
+import profilepic from "../../../public/memeoji.png";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
 
