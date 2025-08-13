1684083fc12171ebb7986c8b3cf8e1ea9bc17914
asallh <ajaypalsallh@gmail.com>
2025-08-12 22:03:39 -0600

    Updated Data

--- src/app/data/projectData.ts
+++ src/app/data/projectData.ts
@@ -36,22 +36,28 @@ export const projects = [
       " Created a webapp that allows users to enter a url and get all page assets and text as a ZIP file. Deployed on Azure AKS",
     techStack: ["React", "Node.js", "Kubernetes"],
   },
-  {
-    title: "Helios-Mercury",
-    details:
-      "Worked with a team of 7 to help develop Helios Mercury, the software to be ran on the Helios Solar car apart of the Solar car Team U of C.",
-    techStack: ["Qt6", "QML", "C++"],
-  },
+  // {
+  //   title: "Helios-Mercury",
+  //   details:
+  //     "Worked with a team of 7 to help develop Helios Mercury, the software to be ran on the Helios Solar car apart of the Solar car Team U of C.",
+  //   techStack: ["Qt6", "QML", "C++"],
+  // },
   {
     title: "Pi Tag Manager",
     details:
-      "Worked with a team of 7 to help develop Helios Mercury, the software to be ran on the Helios Solar car apart of the Solar car Team U of C.",
+      "Developed a Streamlit application that introduced new workflows, enhancing collaboration and technological efficiency across the Strathcona Resources team.",
     techStack: ["Python", "Streamlit", "Databricks", "PostgreSQL"],
   },
+  // {
+  //   title: "Homelab",
+  //   details:
+  //     "Created an NGINX reverse proxy using a Rpi4 connecting to a custom server running Docker containers for various appliactions. Also includes a DIY NAS running TruNas ",
+  //   techStack: ["Rpi4", "NGINX", "NAS", "Linux Server"],
+  // },
   {
-    title: "Homelab",
+    title: "Omnes-Git",
     details:
-      "Created an NGINX reverse proxy using a Rpi4 connecting to a custom server running Docker containers for various appliactions. Also includes a DIY NAS running TruNas ",
-    techStack: ["Rpi4", "NGINX", "NAS", "Linux Server"],
+      "Created a CLI for visualizing Git history and commits through the Command line. ",
+    techStack: ["React", "Ink", "NodeJS", "Typescript"],
   },
 ];
