9c021318d930b9824a4af31870fb976623563779
asallh <ajaypalsallh@gmail.com>
2025-08-20 20:36:10 -0600

    Creating the first set up tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,41 @@
 import Hero from "@/app/views/Hero";
-import { Box } from "@chakra-ui/react";
+import { Box, Tabs } from "@chakra-ui/react";
 import Projects from "./views/Projects";
+import AllProjects from "./views/AllProjects";
+import Blog from "./views/Blog";
+
+function Features() {
+  return (
+    <Tabs.Root defaultValue="featured">
+      <Tabs.List>
+        <Tabs.Trigger value="featured">Featured</Tabs.Trigger>
+        <Tabs.Trigger value="projects">Projects</Tabs.Trigger>
+        <Tabs.Trigger value="blog">Blog</Tabs.Trigger>
+      </Tabs.List>
+      <Tabs.Content value="featured">
+        <Box className="pb-4">
+          <Projects />
+        </Box>
+      </Tabs.Content>
+      <Tabs.Content value="projects">
+        <Box className="pb-4">
+          <AllProjects />
+        </Box>
+      </Tabs.Content>
+      <Tabs.Content value="blog">
+        <Box className="pb-4">
+          <Blog />
+        </Box>
+      </Tabs.Content>
+    </Tabs.Root>
+  );
+}
 
 export default function Home() {
   return (
     <Box>
-        <Hero />
-        <Box className="pb-4">
-          <Projects />
-        </Box>
+      <Hero />
+      <Features />
     </Box>
   );
 }
